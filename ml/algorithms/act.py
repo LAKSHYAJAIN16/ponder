b21368a65d3f24efb88a8d024ae27886f0ce9843
LAKSHYAJAIN16 <minecraftpogtechno@gmail.com>
2023-03-07 09:23:02 +0530

    doritos

--- ml/algorithms/act.py
+++ ml/algorithms/act.py
@@ -1,8 +1,8 @@
 import json 
 
 # Two Files
-FILE_1 = json.load(open(r"D:\Projects\v2\apps\ponder\globalcn.json", "r+"))
-FILE_2 = json.load(open(r"D:\Projects\v2\apps\ponder\american-conservative-fa.json", "r+"))
+FILE_2 = json.load(open(r"D:\Projects\v2\apps\ponder\globalcn.json", "r+"))
+FILE_1 = json.load(open(r"D:\Projects\v2\apps\ponder\american-conservative-fa.json", "r+"))
 
 # Get Keys
 FILE_1_KEYS = FILE_1.keys()
@@ -16,8 +16,9 @@ for key1 in FILE_1_KEYS:
         string += para
     CONTENT_1.append({"content" : string, "title" : content["headline"]})
 
-buf = CONTENT_1[3]["content"]
-buf2 = CONTENT_1[4]["content"]
-search_str = "I will give you two article, separated by tildas. Are both of them on the same topic? Do both of them have opposite views?"
+buf = CONTENT_1[9]["content"]
+buf2 = CONTENT_1[10]["content"]
+search_str = "I wll give you two articles, separated by tildas. Are both of them on the same topic? Do both of them have opposite views?"
 final = search_str + r"'" + buf + r"'  ~  '" + buf2 + r"'"
+print(len(final))
 print(final)
